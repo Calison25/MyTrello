bcbf0c5ff3a186f8d7a513851dd822ca86147491
Calison <calisonsi@gmail.com>
2017-12-18 23:08:34 -0300

    Fecha o DockerFile da Api

---
+++ docker/trello_api/init.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+rm -f /var/run/apache2/apache2.pid
+source /etc/apache2/envvars
+cp /root/Settings.yaml /var/www/agilize/Configuration/Settings.yaml
+touch /tmp/bac
+chmod 777 /tmp/bac
+echo "Init Flow..."
+cd /var/www/agilize/ && rm -rf /var/www/agilize/Data/Temporary/ && ./flow core:setfilepermissions root www-data www-data
+exec apache2 -DFOREGROUND
