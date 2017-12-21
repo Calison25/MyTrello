85baa38d3ec18ad4c904255329a51982096a7754
Calison <calisonsi@gmail.com>
2017-12-20 22:16:31 -0300

    Finaliza o docker da api

--- docker/trello_api/init.sh
+++ docker/trello_api/init.sh
@@ -1,11 +1,9 @@
-#!/bin/bash
-
 set -e
 rm -f /var/run/apache2/apache2.pid
 source /etc/apache2/envvars
-cp /root/Settings.yaml /var/www/agilize/Configuration/Settings.yaml
+cp /root/Settings.yaml /var/www/trello/Configuration/Settings.yaml
 touch /tmp/bac
 chmod 777 /tmp/bac
 echo "Init Flow..."
-cd /var/www/agilize/ && rm -rf /var/www/agilize/Data/Temporary/ && ./flow core:setfilepermissions root www-data www-data
+cd /var/www/trello/ && rm -rf /var/www/trello/Data/Temporary/ && ./flow core:setfilepermissions root www-data www-data
 exec apache2 -DFOREGROUND
