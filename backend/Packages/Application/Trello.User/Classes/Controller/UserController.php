a1c7bd450e2eaf4a3c6cd85b1578ddaf902fa738
Calison <calisonsi@gmail.com>
2017-12-26 20:28:15 -0300

    Cria rota

---
+++ backend/Packages/Application/Trello.User/Classes/Controller/UserController.php
@@ -0,0 +1,21 @@
+<?php
+namespace Trello\User\Controller;
+
+/*
+ * This file is part of the Trello.User package.
+ */
+
+use Neos\Flow\Annotations as Flow;
+use Neos\Flow\Mvc\Controller\ActionController;
+
+class UserController extends ActionController
+{
+    /**
+     *
+     */
+    public function createAction()
+    {
+        echo 'consegui chegar aqui222232323!!!';
+        die;
+    }
+}
