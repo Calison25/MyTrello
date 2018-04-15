d33d567531d76578170e431bb42a684fec3e92b1
calison <calisonsi@gmail.com>
2018-04-15 15:50:43 -0300

    Fazendo tutorial do react

--- frontend/src/components/LadingPage.jsx
+++ frontend/src/components/LadingPage.jsx
@@ -5,12 +5,24 @@ import { Switch, Route } from 'react-router-dom'
 
 class LadingPage extends Component {
     render() {
-        return(
-            <div className="LandingPage">
-              <SignInPath/>
-              <SignUpPath/>
-            </div>
-        );
+        // return(
+        //     <div className="LandingPage">
+        //       <SignInPath/>
+        //       <SignUpPath/>ta
+        //     </div>
+        // );
+        var arrayTest = [];
+
+        arrayTest["testando"] = ["isso funciona!"];
+        arrayTest["testando"].push("isso funciona!2");
+        arrayTest["testando"].push("isso funciona!3");
+        arrayTest["testando"].push("isso funciona!4");
+        arrayTest["testando2"] = ["isso funciona!3"];
+        arrayTest["testando2"].push("isso funciona!4");
+        arrayTest["testando2"].push("isso funciona!5");
+
+        console.log(arrayTest);
+
     }
 }
 
