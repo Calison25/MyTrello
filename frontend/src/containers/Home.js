3e4065a354c78026baf0d27ebd306be1f7e4952c
Calison <calisonsi@gmail.com>
2018-10-07 18:07:58 -0300

    Adiciona react bootstrap

--- frontend/src/containers/Home.js
+++ frontend/src/containers/Home.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
+import Login from "../components/Login";
 
 class Home extends Component {
     render() {
         return (
             <div>
-                Home!!!
+                <link rel="stylesheet" href="https://bootswatch.com/4/sketchy/bootstrap.min.css" type="text/css"></link>
+                <Login />
             </div>
         );
     }
