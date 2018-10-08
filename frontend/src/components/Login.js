fb83c43927458a4dbdc0ba6e7c3914a94b10a7ac
Calison <calisonsi@gmail.com>
2018-10-08 00:18:01 -0300

    Adiciona react bootstrap

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,30 +1,29 @@
 import React, {Component} from 'react';
+import { Button } from 'react-bootstrap';
 import '../style/login.css';
 
 class Login extends Component {
     render() {
         return (
-            <div className="row">
-                <div className="col-md-6 mb-4">
-                    <div className="card near-moon-gradient form-white">
-                        <div className="card-body">
-                            <form>
-                                <h3 className="text-center indigo-text font-bold py-4"><strong>Subscribe</strong></h3>
-                                <div className="md-form">
-                                    <i className="fa fa-user prefix white-text"/>
-                                    <input type="text" id="form35" className="form-control" />
-                                        <label htmlFor="form35">Your name</label>
-                                </div>
-                                <div className="md-form">
-                                    <i className="fa fa-envelope prefix white-text" />
-                                    <input type="text" id="form25" className="form-control" />
-                                        <label htmlFor="form25">Your email</label>
-                                </div>
-                                <div className="text-center py-4">
-                                    <button className="btn btn-indigo">Send <i className="fa fa-paper-plane-o ml-1"></i></button>
-                                </div>
-                            </form>
-                        </div>
+            <div className="col-md-6 mb-4">
+                <div className="card near-moon-gradient form-white">
+                    <div className="card-body">
+                        <form>
+                            <p className="h5 text-center mb-4">Sign in</p>
+                            <div className="md-form">
+                                <i className="fa fa-envelope prefix white-tex"></i>
+                                <input type="text" id="form35" className="form-control" />
+                                    <label htmlFor="form35">Email</label>
+                            </div>
+                            <div className="md-form">
+                                <i className="fa fa-lock prefix grey-tex"></i>
+                                <input type="password" id="form25" className="form-control" />
+                                <label htmlFor="form25">Password</label>
+                            </div>
+                            <div className="text-center py-4">
+                                <Button className="btn btn-indigo">Sign in <i className="fa fa-paper-plane-o ml-1"></i></Button>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
