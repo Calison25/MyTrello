9fca6715979755b0dd0f5243339a2519e15a63e6
Calison <calisonsi@gmail.com>
2018-10-13 13:46:18 -0300

    Corrige autoload

--- backend/Packages/Libraries/composer/autoload_static.php
+++ backend/Packages/Libraries/composer/autoload_static.php
@@ -12,7 +12,6 @@ class ComposerStaticInit860f79d287d8f7dd6cddd6f2624e5c23
         '5255c38a0faeba867671b61dfda6d864' => __DIR__ . '/..' . '/paragonie/random_compat/lib/random.php',
         'c964ee0ededf28c96ebd9db5099ef910' => __DIR__ . '/..' . '/guzzlehttp/promises/src/functions_include.php',
         'a0edc8309cc5e1d60e3047b5df6b7052' => __DIR__ . '/..' . '/guzzlehttp/psr7/src/functions_include.php',
-        'c594688b3441835d5575f3085da4a242' => __DIR__ . '/..' . '/webonyx/graphql-php/src/deprecated.php',
         '37a3dc5111fe8f707ab4c132ef1dbc62' => __DIR__ . '/..' . '/guzzlehttp/guzzle/src/functions_include.php',
     );
 
@@ -28,7 +27,6 @@ class ComposerStaticInit860f79d287d8f7dd6cddd6f2624e5c23
         ),
         'W' => 
         array (
-            'Wwwision\\GraphQL\\' => 17,
             'Webmozart\\Assert\\' => 17,
         ),
         'T' => 
@@ -74,7 +72,6 @@ class ComposerStaticInit860f79d287d8f7dd6cddd6f2624e5c23
             'GuzzleHttp\\Psr7\\' => 16,
             'GuzzleHttp\\Promise\\' => 19,
             'GuzzleHttp\\' => 11,
-            'GraphQL\\' => 8,
             'Gedmo\\' => 6,
         ),
         'D' => 
@@ -103,10 +100,6 @@ class ComposerStaticInit860f79d287d8f7dd6cddd6f2624e5c23
         'Zend\\Code\\' => 
         array (
             0 => __DIR__ . '/..' . '/zendframework/zend-code/src',
-        ),
-        'Wwwision\\GraphQL\\' => 
-        array (
-            0 => __DIR__ . '/../../..' . '/Packages/Application/Wwwision.GraphQL/Classes',
         ),
         'Webmozart\\Assert\\' => 
         array (
@@ -221,10 +214,6 @@ class ComposerStaticInit860f79d287d8f7dd6cddd6f2624e5c23
         'GuzzleHttp\\' => 
         array (
             0 => __DIR__ . '/..' . '/guzzlehttp/guzzle/src',
-        ),
-        'GraphQL\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/webonyx/graphql-php/src',
         ),
         'Gedmo\\' => 
         array (
